e0a4141769a0ea9747ae4b3ec103b4a485a7a64b
黄泉路好走 <39544678+xiaoxucong@users.noreply.github.com>
2021-01-30 19:18:26 +0800

    过滤器

--- 02nio/nio02/src/main/java/io/github/kimmking/gateway/filter/HeaderHttpRequestFilter.java
+++ 02nio/nio02/src/main/java/io/github/kimmking/gateway/filter/HeaderHttpRequestFilter.java
@@ -2,11 +2,30 @@ package io.github.kimmking.gateway.filter;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpRequest;
+
+import java.net.InetSocketAddress;
 
 public class HeaderHttpRequestFilter implements HttpRequestFilter {
 
     @Override
     public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        String uri = fullRequest.uri();
+        String ip = HeaderHttpRequestFilter.getIpAddr(ctx);
+        System.out.println("ip="+ip+"/uri="+uri);
         fullRequest.headers().set("mao", "soul");
     }
+    public static String getIpAddr(ChannelHandlerContext ctx) {
+        String ip = "";
+        try{
+            InetSocketAddress insocket = (InetSocketAddress) ctx.channel().remoteAddress();
+            ip = insocket.getAddress().getHostAddress();
+            System.out.println(ip);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return ip;
+    }
+
 }
